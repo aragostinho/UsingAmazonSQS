398de05cac76add2971f119007310d1a735c14af
Lazaro Fernandes Lima <lazaro.fl@gmail.com>
2014-12-01 15:28:22 -0200

    Returns a message

--- UsingAmazonSQS/ReceiveMessage.cs
+++ UsingAmazonSQS/ReceiveMessage.cs
@@ -32,38 +32,51 @@ namespace UsingAmazonSQS
 
             for (int i = 0; i < total; i++)
             {
-                List<Message> messages = null;
-                messages = GetMessages(QueueFullUrl, sqs);
+                Message message = null;
+                message = GetMessage(QueueFullUrl, sqs);
+
+                Console.WriteLine();
+                if (message == null) break;
+                Console.WriteLine();
+                Console.WriteLine("Message Content");
+                Console.WriteLine(message.Body);
+                Console.WriteLine();
+
+                //delete message after peek it
+                sqs.DeleteMessage(new DeleteMessageRequest() { QueueUrl = QueueFullUrl, ReceiptHandle = message.ReceiptHandle });
+
+
+                Console.WriteLine();
+                Console.WriteLine("Message has been receive!");
             }
 
             Console.ReadKey();
 
         }
 
-        private static List<Message> GetMessages(string QueueFullUrl, AmazonSQSClient sqs)
+        private static Message GetMessage(string QueueFullUrl, AmazonSQSClient sqs)
         {
             ReceiveMessageRequest requestToReceive = new ReceiveMessageRequest(QueueFullUrl);
             ReceiveMessageResponse response = sqs.ReceiveMessage(requestToReceive);
 
-            if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
-                return response.Messages;
+            if (response.HttpStatusCode != System.Net.HttpStatusCode.OK)
+                throw new ApplicationException("Problems in the endpoint communication!");
 
-            throw new ApplicationException("Problems in the endpoint communication!");
+            if (response.Messages.Count > 0)
+                return response.Messages.First();
+            return null;
         }
 
         private static int GetTotalMessages(string QueueFullUrl, AmazonSQSClient sqs)
         {
-            int total = 0;
             GetQueueAttributesRequest queueAttributesRequest = new GetQueueAttributesRequest(QueueFullUrl, new List<string> { "All" });
             GetQueueAttributesResult queueAttributesResult = sqs.GetQueueAttributes(queueAttributesRequest);
 
-            if (queueAttributesResult.HttpStatusCode == System.Net.HttpStatusCode.OK)
-            {
-                total = queueAttributesResult.ApproximateNumberOfMessages;
-            }
-            return total;
+            if (queueAttributesResult.HttpStatusCode != System.Net.HttpStatusCode.OK)
+                throw new ApplicationException("Problems in the endpoint communication!");
+            
+            return queueAttributesResult.ApproximateNumberOfMessages;
         }
-
 
     }
 }
